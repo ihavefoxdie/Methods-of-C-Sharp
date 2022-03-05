8c8d13f29e9948dc51d74bf3ad9bb804b920f027
Vladislav Nekrasov <vladislav.nekrasov123@gmail.com>
2022-03-05 13:22:28 +0300

    Add project files.

---
+++ Methods of C Sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Practicing
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> names = new List<string>();
+            string input;
+
+            Console.WriteLine("To stop, simply type \"Stop!\".");
+            while (true)
+            {
+                Console.Write("What is your name?\nName: ");
+                input = Console.ReadLine();
+                Console.Clear();
+                if (input.Contains("Stop!"))
+                    break;
+                names.Add(input);
+            }
+            Greeting(names);
+        }
+
+        static void Greeting(List<string> args)
+        {
+            for (int i = 0; i < args.Count; i++)
+                Console.WriteLine($"Hello, { args[i]}!");
+        }
+
+    }
+}
